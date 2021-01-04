617a8177f9d2b10d72fffc2a6f56ef141949df39
sagrudd <stephen@mnemosyne.co.uk>
2021-01-04 22:07:21 +0000

    added some basic documentation

--- R/lodestar.R
+++ R/lodestar.R
@@ -2,36 +2,46 @@
 .default_service = "lodestar"
 .default_keyring = "lodestar"
 
-#' @export
-Lodestar <- R6::R6Class(
-  "Lodestar",
-  public = list(
 
-    initialize = function(backend, keyring=.default_keyring, service=.default_service) {
-      # sanity check that the backend is actually an R6 backend from keyring
-    },
-
-    print = function(...) {
-      cat(paste0(private$.getpasswd()))
-    }
-
-  ),
-
-  private = list(
-    .keyring = NA,
-
-    .get_passwd = function() {
-      return(.keyring$get(keyring = keychain_name, service=kr_service, username=kr_username))
-    }
-  )
-)
-
-
+#' R6 Class for abstracting database connections and maintaining lodestar schema
+#'
+#' @description
+#' This class aims to provide some basic minimal functionality for interacting
+#' with a Lodestar schema in a relational database. A key ambition at outset
+#' has been the separation of user credentials and demonstration code for the
+#' preparation of examples and documentation in the vignettes.
+#'
+#' @import R6
 #' @importFrom magrittr %>%
+#' @importFrom DBI dbConnect
+#' @importFrom RPostgres Postgres
 #' @export
 LodestarConn <- R6::R6Class(
   "LodestarConn",
   public = list(
+
+    #' @description
+    #' Creates a new LodestarConn object. This
+    #' initialisation method orchestrates other sanity checking
+    #' of the defined parameters(s) to ensure that the environment is coherent
+    #' and tractable
+    #'
+    #' @param backend keyring backend object used to define username and
+    #' password independently of the vignette code (persistence) - NA by default
+    #' - software will endeavour to select backend on basis of available
+    #' keyring context
+    #' @param keyring which of the available keyrings will hold the connection
+    #' details - `lodestar` by default.
+    #' @param service the name of the database that we'll endeavour to connect
+    #' to - this will be parsed from the backend context - `lodestar` by
+    #' default.
+    #' @param username the user account to be used with the database - NA by
+    #' default; in simpler installations the software will identify the username
+    #' on basis of e.g. service
+    #' @param silent boolean defining whether logging of process should be
+    #' performed (TRUE by default)
+    #' @return the LodestarConn R6 object
+    #'
     initialize = function(backend=NA, keyring=.default_keyring, service=.default_service, username=NA, silent=FALSE) {
       if (is.na(backend)) {
         if (!silent) message(paste0("using keyring settings for LodestarConn"))
@@ -44,8 +54,13 @@ LodestarConn <- R6::R6Class(
       private$.silent = silent
       private$.username = username
       private$.check_backend()
+      invisible()
     },
 
+    #' @description
+    #' Get a database connection corresponding to the passed parameters
+    #'
+    #' @return a DBI connection object
     connection = function() {
       con <- DBI::dbConnect(
         RPostgres::Postgres(),
@@ -120,6 +135,15 @@ LodestarConn <- R6::R6Class(
 )
 
 
+#' Prepare a summary of available keyring annotations
+#'
+#' In a larger SQL environment there is a possibility of 10s of different
+#' RDBMS systems and considerable numbre of database instances - this simple
+#' method prepares a tibble that summarises the available keyring, service and
+#' username information for reminding the available connection possibilities
+#'
+#' @return a tibble
+#'
 #' @export
 lodestar_user_tibble = function() {
   users <- list()
@@ -139,7 +163,20 @@ lodestar_user_tibble = function() {
 }
 
 
-
+#' Inject a username and password into the backend keyring environment
+#'
+#' We are working here to prepare a clean system for the simple and ad hoc
+#' analysis of high dimensional sequence data. This accessory method is intended
+#' to help define the limits of RDBMS, database, username and password required
+#' for connecting to the most appropriate systems.
+#'
+#' @param username username
+#' @param password password
+#' @param service the database to use
+#' @param keyring the keyring where credentials will be stored
+#'
+#' @return backend instance
+#'
 #' @export
 lodestar_creds = function(username, password, service=.default_service, keyring=.default_keyring) {
   backend <- keyring::backend_file$new()
